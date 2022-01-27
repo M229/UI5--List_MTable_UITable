829b1d0cf4d1bb3b3a2d3470b59888ea3290184b
Alexander Artemyev <fakemao@yandex.ru>
2022-01-27 18:35:27 +0300

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"./model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.MyApp.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+		getContentDensityClass : function () {
+			if (!this._sContentDensityClass) {
+				if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+	});
+});
